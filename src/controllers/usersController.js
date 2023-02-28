744256a363ba18785984c92c9b4ab81f56a58511
Carolina <crodriguez@iesrafaelalberti.es>
2023-02-28 18:52:24 +0100

    .

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,9 +1,9 @@
 const usersService = require("../services/usersService");
 
 const addCoursesToUser = (req, res, next) => {
-    const { courses } = req.body;
-    const sessionId = req.cookies.sessionId;
-    const user = usersService.getUserBySessionId(sessionId)
+    // const { courses } = req.body;
+    // const sessionId = req.cookies.sessionId;
+    // const user = usersService.getUserBySessionId(sessionId)
     //user.courses = user.courses.concat(courses)
     //usersService.updateUser(user)
     res.status(200).send("update user").end();
